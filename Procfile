b9e5fd397e1839f5d2570252eebf503e3fe2669b
nirur <niranjanrao049@gmail.com>
2021-02-16 10:05:13 -0800

    configuring heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: venv/bin/activate ; venv/bin/redis-stable/src/redis-server & venv/bin/python manage.py runserver onlinechess1.herokuapp.com:8001
--- server.sh
+++ server.sh
@@ -2,4 +2,4 @@ venv/bin/python manage.py collectstatic --no-input
 
 sudo /etc/init.d/nginx restart
 
-venv/bin/redis-stable/src/redis-server & venv/bin/python manage.py runserver localhost:8001
+venv/bin/redis-stable/src/redis-server & venv/bin/python manage.py runserver onlinechess1.herokuapp.com:8001
