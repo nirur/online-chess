3f8f29bd7459ef9f204823e34c92da0f662aef30
nirur <niranjanrao049@gmail.com>
2021-04-04 12:49:26 -0700

    Making changes for staticfiles and display

---
+++ push.sh
@@ -0,0 +1,6 @@
+python3 manage.py collectstatic --no-input
+python3 manage.py compilestatic
+python3 manage.py collectstatic --no-input
+git add .
+git commit -m "Making changes for staticfiles and display"
+git push -u origin master
--- server.sh
+++
@@ -1,4 +0,0 @@
-python3 manage.py collectstatic --no-input
-python3 manage.py compilestatic
-sudo /etc/init.d/nginx restart
-venv/bin/redis-stable/src/redis-server & daphne onlinechess.asgi:application -b=192.168.86.30 -p=8001 --proxy-headers
