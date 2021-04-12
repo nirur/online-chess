cf141ad49cae6995c7623c33f67835d2bedb297a
nirur <niranjanrao049@gmail.com>
2021-04-11 18:51:24 -0700

    Making changes for staticfiles and display

--- chessapp/urls.py
+++ chessapp/urls.py
@@ -4,10 +4,11 @@ from . import views, consumers
 
 app_name = 'chessapp'
 urlpatterns = [
-               path('', views.login, name='login'), # User side
-               path('play-<int:game_id>', views.game, name='game'), # User side
-               path('home', views.home, name='home'), # User side
-    	       path('create', views.create, name='create'), # Server side
-               path('new', views.new, name='new'), # User side
-	           path('submit', views.submit, name='submit'), # Server side
-	          ]
+    path('', views.login, name='login'),  # User side
+    path('play-<int:game_id>', views.game, name='game'),  # User side
+    path('home', views.home, name='home'),  # User side
+    path('create', views.create, name='create'),  # Server side
+    path('new', views.new, name='new'),  # User side
+    path('accept', views.accept, name='accept'),  # Server Side
+    path('submit', views.submit, name='submit'),  # Server side
+]
