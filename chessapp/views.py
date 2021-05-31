ae92b4c7be03e4aedafd8340a8cbf385073c45e3
nirur <niranjanrao049@gmail.com>
2021-05-31 15:16:24 -0700

    Making changes for staticfiles and display

--- chessapp/views.py
+++ chessapp/views.py
@@ -104,9 +104,6 @@ def home(request):
     reqs_senders = []
     reqs_names = []
     for r in Request.objects.all():
-        print(r.receiver, type(r.receiver))
-        print(user.username, type(user.username))
-        print(r.receiver == user.username)
         if r.receiver == user.username:
             print("hi")
             reqs_ids.append(id(r))
